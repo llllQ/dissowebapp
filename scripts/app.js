7ab674f93123c641d38be2bbb50daa85887a9568
llllQ <matthew.penlington@gmail.com>
2019-02-19 11:59:37 +0000

    reading from database

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,37 @@
 // var testUser = firebase.auth().currentUser;
 // writeUserData(testUser.uid,testUser.displayName,testUser.email);
+
+//foreach item in food database where username is same and food type is same create fooditem html item and populate
+//img, food name, food expiry date and food quantity.
+
+// function readData(foodType){
+//     var userId = firebase.auth().currentUser.uid;
+//     return firebase.database().ref('/inventories/' + userId + '/'+foodType+'/').once('value').then(function(snapshot) {
+//         var foodobj = (snapshot.val());
+//         // console.log(foodobj);
+//     });
+// }
+
+function changeTitle(foodType){
+    var heading = document.getElementById("pageTitle");
+    heading.innerHTML = foodType + " Inventory";
+}
+
+function populateList(foodType){
+    changeTitle(foodType);
+    document.getElementById("nav-toggle").checked = false;
+    var userId = firebase.auth().currentUser.uid;
+    return firebase.database().ref('/inventories/' + userId + '/'+foodType+'/').once('value').then(function(snapshot) {
+        var foodArray = (snapshot.val());
+        var list = document.getElementById("foodList");
+        list.innerHTML = "";
+        foodArray.forEach(function(element) {
+            
+            list.innerHTML += "<li class='foodItem'><img class='foodIcon' src='../resources/carrot.webp'><p class='foodName'>" + element.name +"</p> <p class='foodExp'>"+element.expiry+"</p> <p class='foodQuantity'>"+element.quantity+"</p>";
+            // console.log(element);
+          });
+    });
+
+    // var document.getElementById
+   
+}
--- scripts/firebaseinit.js
+++ scripts/firebaseinit.js
@@ -10,11 +10,15 @@ var config = {
   firebase.initializeApp(config);
 var database = firebase.database();
 
-
 function writeUserData(userId, displayname, email) {
-  console.log("here");
+  // console.log("here");
   firebase.database().ref('users/' + userId).set({
     name: displayname,
     email: email
+  });
+  firebase.database().ref('inventories/' + userId).set({
+    fridge: "a",
+    freezer: "a",
+    pantry: "a"
   });
 }
