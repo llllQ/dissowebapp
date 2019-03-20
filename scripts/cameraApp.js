021639a14c82e857188706e4869e47cab544af0e
llllQ <matthew.penlington@gmail.com>
2019-03-20 13:11:28 +0000

    styling updates to camera
    styling updates to camera button
    removed horizontal flip filter

--- scripts/cameraApp.js
+++ scripts/cameraApp.js
@@ -28,7 +28,7 @@ cameraTrigger.onclick = function() {
     cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
     cameraOutput.src = cameraSensor.toDataURL("image/webp");
     cameraOutput.classList.add("taken");
-    // track.stop();
+    track.stop();
 };
 
 // Start the video stream when the window loads
